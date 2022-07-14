936fb3abbda981e49909d19820ef5d8990289d35
Truc Phan <trucphan@my.unt.edu>
2022-07-14 05:43:52 -0500

    add ui for the game

--- script.js
+++ script.js
@@ -1,53 +1,114 @@
 function computerPlay(){
     let num = Math.floor(Math.random()*(3-1+1)+1);
     if (num === 1){
-        return "Rock";
+        return "rock";
     }else if (num === 2){
-        return "Paper";
+        return "paper";
     }
-    return "Scissors"
+    return "scissors"
 }
+let resultMess = document.querySelector('.result');
+let playerDisplay = document.querySelector('.player-score');
+let computerDisplay = document.querySelector('.computer-score');
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase()
+    let player = playerSelection
+    let computer = computerSelection
+
     if (player === computer){
-        return "tie game!!";
+        resultMess.textContent = "tie game!!";
     }
 
-    if (player === "rock"){
+    if (player == "rock"){
         if (computer === "paper"){
-            return "You Lose! Paper beats Rock";
+           resultMess.textContent = "You Lose! Paper beats Rock";
+           computerScore +=1;
+           computerDisplay.textContent = `Computer: ${computerScore}`;
         }
         if (computer === "scissors"){
-            return "You Win! Rock beats Scissors";
+            resultMess.textContent = "You Win! Rock beats Scissors";
+            playerScore +=1; 
+            playerDisplay.textContent = `You: ${playerScore}`;
         }
     }
 
     if (player === "paper"){
         if (computer === "rock"){
-            return "You Win! Paper beats Rock";
+            resultMess.textContent = "You Win! Paper beats Rock";
+            playerScore +=1;
+            playerDisplay.textContent = `You: ${playerScore}`;
         }
         if (computer === "scissors"){
-            return "You Lose! Scissors beat Paper";
+            resultMess.textContent = "You Lose! Scissors beat Paper";
+            computerScore +=1;
+            computerDisplay.textContent = `Computer: ${computerScore}`;
         }
     }
 
     if (player === "scissors"){
         if (computer === "paper"){
-            return "You Win! Scissors beat Paper";
+            resultMess.textContent = "You Win! Scissors beat Paper";
+            playerScore +=1;
+            playerDisplay.textContent = `You: ${playerScore}`;
         }
         if (computer === "rock"){
-            return "You Lose! Rock beats Scissors";
+            resultMess.textContent = "You Lose! Rock beats Scissors";
+            computerScore +=1;
+            computerDisplay.textContent = `Computer: ${computerScore}`;
         }
+    } 
+    
+}
+function endGame(){
+    if (computerScore == 5){
+        resultMess.textContent = ":( You lost the game!"
+        disableButtons();
     }
+    if (playerScore == 5){
+        resultMess.textContent = "Congrats! You woooonnn the gameeee!"
+        disableButtons();
+    }
+    
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){    
-        let player = prompt("Rock, Paper or Scissors? ");
-        let computer = computerPlay();
-        console.log(playRound(player,computer));
-    }
+function reset(){
+    computerScore = 0;
+    playerScore = 0;
+    playerDisplay.textContent = `You: ${playerScore}`;
+    computerDisplay.textContent = `Computer: ${computerScore}`;
+    enableButtons();
 }
-console.log(game());
+
+const resetBtn = document.getElementById('reset-btn');
+
+
+const rockBtn = document.getElementById('r-btn');
+const paperBtn = document.getElementById('p-btn');
+const scissorsBtn = document.getElementById('s-btn');
+
+rockBtn.addEventListener('click', () => play('rock'));
+paperBtn.addEventListener('click', () => play('paper'));
+scissorsBtn.addEventListener('click', () => play('scissors'));
+
+function disableButtons(){
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+function enableButtons(){
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+}
+function play(player){
+    const computer = computerPlay();
+    playRound(player,computer);
+    endGame();
+    resetBtn.addEventListener('click', reset);
+}
+
+
+
+  
